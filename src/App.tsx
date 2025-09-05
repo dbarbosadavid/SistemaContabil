de9d68762e0f63f39072db24e7bbae1026a2cf3f
dbarbosadavid <david.canal123@gmail.com>
2025-09-05 19:39:27 -0300

    testes2

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import './App.css'
 import { BrowserRouter } from "react-router-dom";
-import Nav from './components/Nav';
 import Body from './components/Body';
 
 function App() {
 
   return (
     <BrowserRouter>
-      
+
       <Body />
       <footer style={{ left: 0, bottom: 0, width: "100%",marginTop: "2rem", padding: "1rem", textAlign: "center", boxShadow: "0 -2px 8px rgba(0,0,0,0.05)", position: "fixed"}}>
         <small>&copy; {new Date().getFullYear()} Sistema Contábil. Todos os direitos reservados.</small>
