049d73723baf95e77e740167119a897876f292bd
dbarbosadavid <david.canal123@gmail.com>
2025-09-12 20:29:53 -0300

    Muitas coisas a arrumar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import './App.css'
 import { BrowserRouter } from "react-router-dom";
 import Body from './components/Body';
+import Nav from './components/Nav';
 
 function App() {
 
   return (
     <BrowserRouter>
-
+      <Nav />
       <Body />
       <footer style={{ left: 0, bottom: 0, width: "100%",marginTop: "2rem", padding: "1rem", textAlign: "center", boxShadow: "0 -2px 8px rgba(0,0,0,0.05)", position: "fixed"}}>
         <small>&copy; {new Date().getFullYear()} Sistema Contábil. Todos os direitos reservados.</small>
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,22 +1,47 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
+function validaLogin(login: String, senha: String){
+    const preLogin = 'david';
+    const preSenha = '123' 
 
+    if (preLogin == login && preSenha == senha){
+        return true
+    }
+    else {
+        return false
+    }
+}
 // toDO: referenciar a página de login na rota e no nav
 const Login: React.FC = () => {
+    const navigate = useNavigate();
+    const [user, setUser] = useState("");
+    const [senha, setSenha] = useState("");
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        navigate("/page/dashboard");
+    };
+    
     return (
         <>
             <h1>Login</h1>
-            <form>
-                <label>E-mail:  </label>
-                <input type="email" required /><br></br>
-
-                <label>Senha:  </label>
-                <input type="password" required /><br></br>
-
-                <button type="submit" onClick={() => {
-                    <Link to="/dashboard"></Link>
-                }}>Entrar</button>
+            <form onSubmit={handleSubmit}>
+                <input
+                    placeholder="Usuário"
+                    value={user}
+                    onChange={(e) => setUser(e.target.value)}
+                />
+                <br />
+                <input
+                    placeholder="Senha"
+                    value={senha}
+                    onChange={(e) => setSenha(e.target.value)}
+                />
+                <br />
+                <button type="submit">Entrar</button>
             </form>
         </>
     )
